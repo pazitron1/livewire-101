b227cc7fd5b63c5a1f0915522d9142c1ea44cb40
Anton Sirik <anton.sirik@mccann.com>
2020-10-14 09:03:17 +0100

    Added search dropdown using itunes api and feature test

---
+++ app/Http/Livewire/SearchDropdown.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+class SearchDropdown extends Component
+{
+    public $search;
+    public $searchResults = [];
+
+    public function updatedSearch($newValue)
+    {
+        $response = Http::get('https://itunes.apple.com/search/?term='. $this->search .'&limit=10');
+        $this->searchResults = $response->json()['results'];
+    }
+
+    public function render()
+    {
+        return view('livewire.search-dropdown');
+    }
+}
